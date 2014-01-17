74bb4bb595758159257554640a8288278407e3ea
Sam Ritchie <sritchie09@gmail.com>
2014-01-17 11:27:52 -0700

    remove tests

--- test/liberator_friend/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns liberator-friend.core-test
-  (:require [clojure.test :refer :all]
-            [liberator-friend.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
